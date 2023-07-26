bf17861e9a9ff8b2f332bc608e15741b2b96c436
Jennyads <jennycristina.1998@gmail.com>
2023-07-25 21:11:46 -0300

    tweaks not found and header

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,19 +3,26 @@ import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
+import Footer from 'components/Footer';
+import Sobre from 'pages/Sobre';
+import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className='container'>
         <Router>
             <Menu />
         <Routes>
             <Route path='/' element={<PaginaPadrao />}>
                 <Route index element={<Inicio />} />
                 <Route path='cardapio' element={<Cardapio />} />
+                <Route path='sobre' element={<Sobre />} />
             </Route> 
-            
+            <Route path='prato/:id' element={<Prato />} />
+            <Route path='*' element={<NotFound />} />
         </Routes>
+        <Footer />
         </Router>
     </main>
   );
